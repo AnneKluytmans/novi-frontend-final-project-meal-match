af621b4ae64db04b69103711e71e8510bfb06fd5
AnneKluytmans <annekluytmans1@gmail.com>
2024-07-18 13:43:13 +0200

    Setup react routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/homePage/Home.jsx';
+import SignIn from './pages/signInPage/SignIn.jsx';
+import SignUp from './pages/signUpPage/SignUp.jsx';
+import RecipeQuiz from './pages/recipeQuizPage/RecipeQuiz.jsx';
+import IngredientSearch from './pages/IngredientSearchPage/IngredientSearch.jsx';
+import AllRecipes from './pages/allRecipesPage/AllRecipes.jsx';
+import RecipeDetails from './pages/recipeDetailsPage/RecipeDetails.jsx';
+import Profile from './pages/profilePage/Profile.jsx';
+import FavoriteRecipes from './pages/favoriteRecipesPage/FavoriteRecipes.jsx';
+import Contact from './pages/contactPage/Contact.jsx';
+import Faq from './pages/faqPage/Faq.jsx';
+import TermsAndPolicy from './pages/termsAndPolicyPage/TermsAndPolicy.jsx';
+import NotFound from './pages/notFoundPage/NotFound.jsx';
 import './App.css';
 
 function App() {
   return (
-    <>
-    </>
+      <>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/sign-in" element={<SignIn/>}/>
+            <Route path="/sign-up" element={<SignUp/>}/>
+            <Route path="/recipe-quiz" element={<RecipeQuiz/>}/>
+            <Route path="/ingredient-search" element={<IngredientSearch/>}/>
+            <Route path="/all-recipes" element={<AllRecipes/>}/>
+            <Route path="/recipe-details/:id" element={<RecipeDetails/>}/>
+            <Route path="/favorite-recipes" element={<FavoriteRecipes/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/faq" element={<Faq/>}/>
+            <Route path="/terms-and-policy" element={<TermsAndPolicy/>}/>
+            <Route path="*" element={<NotFound/>}/>
+          </Routes>
+        </main>
+      </>
   )
 }
 
