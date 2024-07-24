8b2963462fc3d64e0a45e49e3c84b6dfc3c10a07
AnneKluytmans <annekluytmans1@gmail.com>
2024-07-24 17:36:05 +0200

    Implemented Sign Up page with Authentication Form

--- src/pages/signUpPage/SignUp.jsx
+++ src/pages/signUpPage/SignUp.jsx
@@ -1,10 +1,27 @@
+import AuthForm from '../../components/form/authForm/AuthForm.jsx';
+import UsernameField from '../../components/form/usernameField/UsernameField.jsx';
+import EmailField from '../../components/form/emailField/EmailField.jsx';
+import PasswordField from '../../components/form/passwordField/PasswordField.jsx';
+import ConfirmPasswordField from '../../components/form/passwordField/ConfirmPasswordField.jsx';
 import './SignUp.css';
+
 
 function SignUp() {
     return (
-        <>
-            <h1>Sign Up</h1>
-        </>
+        <div className="sign-up-section outer-content-container">
+            <AuthForm
+                title="Sign up to Meal Match to save all your favorite recipes in one place!"
+                buttonText="Sign up"
+                primaryLinkText="Already a member?"
+                primaryLink="/sign-in"
+                primaryLinkName="Sign In"
+            >
+                <UsernameField />
+                <EmailField />
+                <PasswordField />
+                <ConfirmPasswordField />
+            </AuthForm>
+        </div>
     );
 }
 
