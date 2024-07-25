c5eb850c1c464bfd712dc7841d9d7d65a79a63e2
AnneKluytmans <annekluytmans1@gmail.com>
2024-07-25 16:41:39 +0200

    Imported API key's and urls in Api config file

---
+++ src/constants/apiConfig.js
@@ -0,0 +1,7 @@
+// API key's
+export const API_KEY_AUTH = import.meta.env.VITE_API_KEY_AUTH;
+export const API_KEY_SPOONACULAR = import.meta.env.VITE_API_KEY_SPOONACULAR;
+
+// API request url's
+export const API_URL_AUTH = import.meta.env.VITE_API_URL_AUTH;
+export const API_URL_SPOONACULAR = import.meta.env.VITE_API_URL_SPOONACULAR;
