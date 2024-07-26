aa81c5240fc0fddded58ca93a70142154ed57a29
AnneKluytmans <annekluytmans1@gmail.com>
2024-07-26 10:56:07 +0200

    Created state in AuthContext and implemented private routing in App.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,10 +1,17 @@
 import { createContext } from 'react';
+import { useState } from 'react';
 
 export const AuthContext = createContext({});
 
 function AuthContextProvider( { children } ) {
-    const authContextData = {
+    const [ isAuth, setIsAuth ] = useState({
         isAuth: false,
+        user: null,
+    });
+
+    const authContextData = {
+        isAuth: isAuth.isAuth,
+        user: isAuth.user,
     }
 
     return (
