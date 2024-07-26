ffd7453d0c795cab97e6610e8756dee4e264bb6a
AnneKluytmans <annekluytmans1@gmail.com>
2024-07-26 10:34:59 +0200

    Places hardcoded API URL's in apiConfig.js

--- src/constants/apiConfig.js
+++ src/constants/apiConfig.js
@@ -3,5 +3,5 @@ export const API_KEY_AUTH = import.meta.env.VITE_API_KEY_AUTH;
 export const API_KEY_SPOONACULAR = import.meta.env.VITE_API_KEY_SPOONACULAR;
 
 // API request url's
-export const API_URL_AUTH = import.meta.env.VITE_API_URL_AUTH;
-export const API_URL_SPOONACULAR = import.meta.env.VITE_API_URL_SPOONACULAR;
+export const API_URL_AUTH = "https://api.datavortex.nl/mealmatch"
+export const API_URL_SPOONACULAR = "https://api.spoonacular.com"
