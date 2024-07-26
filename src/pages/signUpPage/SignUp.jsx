e09a71b7bc8d747e3bc258b33d1db47c40359f3a
AnneKluytmans <annekluytmans1@gmail.com>
2024-07-26 15:39:06 +0200

    Implemented sign up functionality

--- src/pages/signUpPage/SignUp.jsx
+++ src/pages/signUpPage/SignUp.jsx
@@ -1,30 +1,81 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from "axios";
 import AuthForm from '../../components/form/authForm/AuthForm.jsx';
 import UsernameField from '../../components/form/usernameField/UsernameField.jsx';
 import EmailField from '../../components/form/emailField/EmailField.jsx';
 import PasswordField from '../../components/form/passwordField/PasswordField.jsx';
 import ConfirmPasswordField from '../../components/form/passwordField/ConfirmPasswordField.jsx';
+import Loader from '../../components/misc/loader/Loader.jsx';
+import ErrorMessage from '../../components/misc/errorMessage/ErrorMessage.jsx';
+import ConfirmMessage from '../../components/misc/confirmMessage/ConfirmMessage.jsx';
+import { API_KEY_AUTH, API_URL_AUTH } from '../../constants/apiConfig.js';
 import './SignUp.css';
 
 
 function SignUp() {
-    function handleFormSubmit(data) {
+    const [error, toggleError] = useState(false);
+    const [loading, toggleLoading] = useState(false);
+    const [succesMessage, toggleSuccesMessage] = useState(false);
+
+    const navigate = useNavigate();
+
+    async function handleFormSubmit(data) {
         console.log(data);
+        toggleError(false);
+        toggleLoading(true);
+        toggleSuccesMessage(false);
+
+        try {
+            await axios.post(`${API_URL_AUTH}/users`, {
+                username: data.username,
+                email: data.email,
+                password: data.password,
+                info: "",
+                authorities: [
+                    {
+                        authority: "user"
+                    }
+                ]
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-API-Key': API_KEY_AUTH,
+                }
+            });
+
+            console.log('Registration successful');
+            toggleSuccesMessage(true);
+
+            setTimeout(() => {
+                navigate('/sign-in');
+            }, 3000);
+        } catch (e) {
+            console.error(e);
+            toggleError(true);
+        } finally {
+            toggleLoading(false);
+        }
     }
 
     return (
         <section className="sign-up-section outer-content-container">
-            <AuthForm
-                title="Sign up to Meal Match to save all your favorite recipes in one place!"
-                buttonText="Sign up"
-                handleFormSubmit={handleFormSubmit}
-                primaryLink={<p>Already a member? <Link to="/sign-in">Sign In</Link></p>}
-            >
-                <UsernameField />
-                <EmailField />
-                <PasswordField />
-                <ConfirmPasswordField />
-            </AuthForm>
+            <div className="inner-content-container__column">
+                {loading && <Loader text="Cooking up a fresh new account for you... 🍳✨"/>}
+                {error && <ErrorMessage message="Something went wrong while setting up your profile...🍰❌ Please try again later!"/>}
+                {succesMessage && <ConfirmMessage message="Hooray! Your profile is now live and sizzling! 🥳🔥 You’re all set to explore!" autoClose={2500}/>}
+                <AuthForm
+                    title="Sign up to Meal Match to save all your favorite recipes in one place!"
+                    buttonText="Sign up"
+                    handleFormSubmit={handleFormSubmit}
+                    primaryLink={<p>Already a member? <Link to="/sign-in">Sign In</Link></p>}
+                >
+                    <UsernameField />
+                    <EmailField />
+                    <PasswordField />
+                    <ConfirmPasswordField />
+                </AuthForm>
+            </div>
         </section>
     );
 }
