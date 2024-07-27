523f3f12a4a82e510920d8a2163481fb2ed0e35f
AnneKluytmans <annekluytmans1@gmail.com>
2024-07-27 17:50:58 +0200

    Implemented login and fetchUserData functionalities inAuthContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,8 @@
 import { createContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
+import axios from 'axios';
+import { API_KEY_AUTH, API_URL_AUTH } from '../constants/apiConfig.js';
 
 
 export const AuthContext = createContext({});
@@ -10,12 +13,49 @@ function AuthContextProvider( { children } ) {
         user: null,
     });
 
+    const navigate = useNavigate();
+
     function login(JWT) {
         console.log(JWT);
         localStorage.setItem('token', JWT);
 
         const decodedToken = jwtDecode(JWT);
         console.log(decodedToken);
+
+        void fetchUserData(decodedToken.sub, JWT, '/');
+    }
+
+    async function fetchUserData(id, token, redirectUrl) {
+        try {
+            const response = await axios.get(`${API_URL_AUTH}/users/${ id }`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Api-Key': API_KEY_AUTH,
+                    Authorization: `Bearer ${ token }`,
+                },
+            });
+
+            console.log(response.data);
+            setIsAuth({
+                isAuth: true,
+                user: {
+                    username: response.data.username,
+                    email: response.data.email,
+                }
+            });
+
+            if ( redirectUrl ) {
+                setTimeout(() => {
+                    navigate( redirectUrl );
+                }, 3000);
+            }
+        } catch (e) {
+            console.error(e);
+            setIsAuth({
+                isAuth: false,
+                user: null,
+            });
+        }
     }
 
     const authContextData = {
