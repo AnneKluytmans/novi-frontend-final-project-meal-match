734917ce8f2a7c5aa87008a36b97be427954c79d
AnneKluytmans <annekluytmans1@gmail.com>
2024-07-30 16:59:48 +0200

    Implemented Home Page with feature cards

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,7 +1,11 @@
 import { useNavigate } from 'react-router-dom';
+import { MagnifyingGlass } from '@phosphor-icons/react';
+import InspirationIcon from '../../assets/icons/inspiration-icon.svg?react';
+import IngredientsIcon from '../../assets/icons/ingredients-icon.svg?react';
 import dishes from '../../assets/variety-of-dishes.jpg';
 import SectionDivider from '../../components/misc/sectionDivider/SectionDivider.jsx';
 import Button from '../../components/buttons/button/Button.jsx';
+import FeatureCard from '../../components/cards/featureCard/FeatureCard.jsx';
 import './Home.css';
 
 
@@ -28,7 +32,33 @@ function Home() {
             <section className="features-section outer-content-container">
                 <div className="inner-content-container__column">
                     <SectionDivider title="Features"/>
-                    <h3>Features</h3>
+                    <div className="features__container">
+                        <FeatureCard
+                            icon={<InspirationIcon className="feature-card__icon"/>}
+                            title="Recipe Quiz"
+                            description="Answer a few quick questions to receive personalized recipe suggestions tailored
+                            to your tastes and preferences."
+                            buttonText="Start quiz"
+                            redirectUrl="/recipe-quiz"
+                        />
+                        <FeatureCard
+                            icon={<IngredientsIcon className="feature-card__icon"/>}
+                            title="Ingredient Search"
+                            description="Use the ingredient search feature to find recipes based on what you have in your
+                            pantry and fridge."
+                            buttonText="Search recipes"
+                            redirectUrl="/ingredient-search"
+                        />
+                        <FeatureCard
+                            icon={<MagnifyingGlass className="feature-card__icon" weight="bold"/>}
+                            title="All recipes"
+                            description="Browse through our delightful recipe collection to discover the perfect dish
+                            for any mood or occasion."
+                            buttonText="Explore recipes"
+                            redirectUrl="/all-recipes"
+                        />
+                    </div>
+
                 </div>
             </section>
             <section className="quote-section outer-content-container">
