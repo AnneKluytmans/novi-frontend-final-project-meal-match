3f3b6d9a1ad62dcfcd5ca9131e01c8e3c56d9c94
AnneKluytmans <annekluytmans1@gmail.com>
2024-08-12 10:48:38 +0200

    Implemented Recipe Quiz page with header and global structure

--- src/pages/recipeQuizPage/RecipeQuiz.jsx
+++ src/pages/recipeQuizPage/RecipeQuiz.jsx
@@ -1,9 +1,24 @@
+import InspirationIcon from '../../assets/icons/inspiration-icon.svg?react';
+import Header from '../../components/header/Header.jsx';
+import SectionDivider from '../../components/misc/sectionDivider/SectionDivider.jsx';
 import './RecipeQuiz.css';
+
+
 
 function RecipeQuiz() {
     return (
       <>
-        <h1>Recipe Quiz</h1>
+          <Header
+              title="Recipe Quiz"
+              subtitle="Take our recipe quiz for inspiring recipe ideas matching your mood, taste and preferences."
+              icon={<InspirationIcon className="header__icon"/>}
+          />
+          <section className="outer-content-container">
+              <div className="inner-content-container__column">
+                  <SectionDivider title="Recipe Quiz"/>
+                  <h3>Recipe Quiz</h3>
+              </div>
+          </section>
       </>
     );
 }
