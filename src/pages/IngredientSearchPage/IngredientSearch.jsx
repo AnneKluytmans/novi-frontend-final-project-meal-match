e02cfaecec52f4d8932ff3775f75f02957d410d8
AnneKluytmans <annekluytmans1@gmail.com>
2024-08-15 12:32:33 +0200

    Implemented Ingredient Search page with header and global structure

--- src/pages/IngredientSearchPage/IngredientSearch.jsx
+++ src/pages/IngredientSearchPage/IngredientSearch.jsx
@@ -1,9 +1,21 @@
+import IngredientsIcon from '../../assets/icons/ingredients-icon.svg?react';
+import Header from '../../components/header/Header.jsx';
+import SectionDivider from '../../components/misc/sectionDivider/SectionDivider.jsx';
 import './IngredientSearch.css';
 
 function IngredientSearch() {
     return (
       <>
-        <h1>Ingredient Search</h1>
+        <Header
+            title="Ingredient Search"
+            subtitle="Discover delicious recipes with what you have on hand. Just enter your ingredients."
+            icon={<IngredientsIcon className="header__icon"/>}
+        />
+        <section className="outer-content-container">
+            <div className="inner-content-container__column">
+                <SectionDivider title="Search by ingredients" />
+            </div>
+        </section>
       </>
     );
 }
