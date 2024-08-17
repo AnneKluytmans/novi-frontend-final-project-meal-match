8bc959830575671396c270756b552543e533686c
AnneKluytmans <annekluytmans1@gmail.com>
2024-08-17 11:35:07 +0200

    Implemented All Recipes page with header and global structure

--- src/pages/allRecipesPage/AllRecipes.jsx
+++ src/pages/allRecipesPage/AllRecipes.jsx
@@ -1,9 +1,22 @@
+import { MagnifyingGlass } from '@phosphor-icons/react';
+import Header from '../../components/header/Header.jsx';
+import SectionDivider from '../../components/misc/sectionDivider/SectionDivider.jsx';
 import './AllRecipes.css';
+
 
 function AllRecipes() {
     return (
         <>
-            <h1>All recipes</h1>
+            <Header
+                title="All Recipes"
+                subtitle="Explore a world of flavors. Browse, filter, and find your perfect recipe match."
+                icon={<MagnifyingGlass stroke="#D8F499" strokeWidth={4} className="header__icon"/>}
+            />
+            <section className="outer-content-container">
+                <div className="inner-content-container__column">
+                    <SectionDivider title="Explore Recipes"/>
+                </div>
+            </section>
         </>
     );
 }
