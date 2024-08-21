c20c361d63b6828ad4c58cbc103e6366b913ef5f
AnneKluytmans <annekluytmans1@gmail.com>
2024-08-21 15:16:26 +0200

    Implemented Favorite Recipes page with header and globale structure

--- src/pages/favoriteRecipesPage/FavoriteRecipes.jsx
+++ src/pages/favoriteRecipesPage/FavoriteRecipes.jsx
@@ -1,9 +1,22 @@
+import { Heart } from '@phosphor-icons/react';
+import Header from '../../components/header/Header.jsx';
+import SectionDivider from '../../components/misc/sectionDivider/SectionDivider.jsx';
 import './FavoriteRecipes.css'
+
 
 function FavoriteRecipes() {
     return (
       <>
-        <h1>Favorite Recipes</h1>
+          <Header
+              title="Favorite Recipes"
+              subtitle="All your favorite recipes all in one place."
+              icon={<Heart className="header__icon"/>}
+          />
+          <section className="outer-content-container">
+              <div className="inner-content-container__column">
+                  <SectionDivider title="Favorite Recipes"/>
+              </div>
+          </section>
       </>
     );
 }
