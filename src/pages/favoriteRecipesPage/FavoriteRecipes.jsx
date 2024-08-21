1538236cfd7667123c06e7f4bbbbd9d88a3e2934
AnneKluytmans <annekluytmans1@gmail.com>
2024-08-21 15:40:35 +0200

    Implemented Favorite Recipes Page with API request to fetch the users favorite recipes

--- src/pages/favoriteRecipesPage/FavoriteRecipes.jsx
+++ src/pages/favoriteRecipesPage/FavoriteRecipes.jsx
@@ -1,10 +1,74 @@
+import { useContext, useState, useEffect } from 'react';
+import axios from 'axios';
 import { Heart } from '@phosphor-icons/react';
+import { FavContext } from '../../context/FavContext.jsx';
 import Header from '../../components/header/Header.jsx';
 import SectionDivider from '../../components/misc/sectionDivider/SectionDivider.jsx';
+import { API_KEY_EDAMAM, API_ID_EDAMAM, API_URL_EDAMAM } from '../../constants/apiConfig.js';
 import './FavoriteRecipes.css'
 
 
 function FavoriteRecipes() {
+    const [recipes, setRecipes] = useState(null);
+    const [error, toggleError] = useState(false);
+    const [loading, toggleLoading] = useState(false);
+
+    const { favoriteRecipes, error: contextError, loading: contextLoading } = useContext(FavContext);
+
+    useEffect( () => {
+        // Fetches favorite recipes on component mount or updated favoriteRecipes
+        const controller = new AbortController();
+
+        async function fetchFavoriteRecipes() {
+            toggleError(false);
+            toggleLoading(true);
+
+            try {
+                const response = favoriteRecipes.map(id =>
+                    axios.get(`${API_URL_EDAMAM}/${id}`, {
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        params: {
+                            type: 'public',
+                            app_id: API_ID_EDAMAM,
+                            app_key: API_KEY_EDAMAM,
+                        },
+                        signal: controller.signal,
+                    })
+                );
+
+                const responses = await Promise.all(response);
+                const fetchedRecipes = responses.map(response => response.data.recipe);
+
+                console.log('Favorite Recipes are fetched:', fetchedRecipes);
+                setRecipes(fetchedRecipes);
+            } catch (e) {
+                if (axios.isCancel(e)) {
+                    console.log('Request canceled', e.message);
+                } else {
+                    console.log('Error during fetching recipe details:', e);
+                    toggleError(true);
+                }
+            } finally {
+                toggleLoading(false);
+            }
+        }
+
+        if (favoriteRecipes.length > 0) {
+            void fetchFavoriteRecipes();
+        } else {
+            setRecipes(null);
+        }
+
+        // Cancels ongoing Axios requests on component unmount
+        return function cleanup() {
+            console.log('Unmount effect is triggered. Abort ongoing axios requests');
+            controller.abort();
+        }
+    }, [favoriteRecipes]);
+
+
     return (
       <>
           <Header
