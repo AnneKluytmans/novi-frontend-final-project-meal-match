5d7b2f87101d38bd84dd07e93c584606690e7ba5
AnneKluytmans <annekluytmans1@gmail.com>
2024-08-22 12:44:25 +0200

    Implemented contact page with contact form

--- src/pages/contactPage/Contact.jsx
+++ src/pages/contactPage/Contact.jsx
@@ -1,9 +1,23 @@
+import { useForm, FormProvider } from 'react-hook-form';
 import { ChatCircleText } from '@phosphor-icons/react';
 import Header from '../../components/header/Header.jsx';
 import SectionDivider from '../../components/misc/sectionDivider/SectionDivider.jsx';
+import TextField from '../../components/form/TextField/TextField.jsx';
+import EmailField from '../../components/form/emailField/EmailField.jsx';
+import TextArea from '../../components/form/textArea/TextArea.jsx';
+import Button from '../../components/buttons/button/Button.jsx';
 import './Contact.css';
 
+
 function Contact() {
+    const methods = useForm({
+        mode: 'onTouched',
+    });
+
+    function handleFormSubmit(data) {
+        console.log(data);
+    }
+
     return (
         <>
             <Header
@@ -14,6 +28,26 @@ function Contact() {
             <section className="outer-content-container">
                 <div className="inner-content-container__column">
                     <SectionDivider title="Contact" />
+                    <div className="contact-form__container">
+                        <h4 className="default-text-restrictor">
+                            Leave your question or message below
+                            <br/>
+                            We’re here to help you
+                        </h4>
+                        <FormProvider {...methods}>
+                            <form onSubmit={methods.handleSubmit(handleFormSubmit)} noValidate>
+                                <TextField name="name" label="Name" />
+                                <EmailField />
+                                <TextArea />
+                                <Button
+                                    type="submit"
+                                    className="btn btn__default"
+                                >
+                                    Contact us
+                                </Button>
+                            </form>
+                        </FormProvider>
+                    </div>
                 </div>
             </section>
         </>
