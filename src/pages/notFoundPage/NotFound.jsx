bf454af37bbbc63670323e06d124b0b986866981
AnneKluytmans <annekluytmans1@gmail.com>
2024-08-22 15:19:27 +0200

    Implemented Not Found page with header and global structure

--- src/pages/notFoundPage/NotFound.jsx
+++ src/pages/notFoundPage/NotFound.jsx
@@ -1,10 +1,22 @@
+import { SmileyXEyes} from '@phosphor-icons/react';
+import Header from '../../components/header/Header.jsx';
+import SectionDivider from '../../components/misc/sectionDivider/SectionDivider.jsx';
 import './NotFound.css';
 
 
 function NotFound() {
     return (
         <>
-           <h1>Not Found</h1>
+            <Header
+                title="Oops! Not Found..."
+                subtitle="This page doesn't exist. You've taken a wrong path in your culinary adventure."
+                icon={<SmileyXEyes className="header__icon--big"/>}
+            />
+            <section className="outer-content-container">
+                <div className="inner-content-container__column">
+                    <SectionDivider title="Not Found"/>
+                </div>
+            </section>
         </>
     );
 }
