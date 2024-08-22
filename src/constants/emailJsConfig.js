920430430b8575e85648bd39086639f0318f58cd
AnneKluytmans <annekluytmans1@gmail.com>
2024-08-22 14:50:37 +0200

    Added Email Js ID's to .env, .env.dist and emailJsConfig.js

---
+++ src/constants/emailJsConfig.js
@@ -0,0 +1,4 @@
+// EmailJs ID's
+export const EMAILJS_SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+export const EMAILJS_TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+export const EMAILJS_USER_ID = import.meta.env.VITE_EMAILJS_USER_ID;
