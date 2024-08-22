10aad7421e36c24c572f5e10483f12b403bbe8eb
AnneKluytmans <annekluytmans1@gmail.com>
2024-08-22 11:17:46 +0200

    Implemented Contact page with header and global structure

--- src/pages/contactPage/Contact.jsx
+++ src/pages/contactPage/Contact.jsx
@@ -1,9 +1,22 @@
+import { ChatCircleText } from '@phosphor-icons/react';
+import Header from '../../components/header/Header.jsx';
+import SectionDivider from '../../components/misc/sectionDivider/SectionDivider.jsx';
 import './Contact.css';
 
 function Contact() {
     return (
       <>
-        <h1>Contact</h1>
+          <Header
+              title="Contact"
+              subtitle="Contact us below. We’re here to help with all your foodie questions, culinary support and more."
+              icon={<ChatCircleText className="header__icon"/>}
+          />
+          <section className="outer-content-container">
+              <div className="inner-content-container__column">
+                  <SectionDivider title="Contact" />
+                  <h3>Contact form</h3>
+              </div>
+          </section>
       </>
     );
 }
