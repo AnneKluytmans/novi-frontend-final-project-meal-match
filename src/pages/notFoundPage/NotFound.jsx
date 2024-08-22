3f633650dfcef04e3a121044c787b6990da5e33f
AnneKluytmans <annekluytmans1@gmail.com>
2024-08-22 15:39:51 +0200

    Implemented Not Found page with content

--- src/pages/notFoundPage/NotFound.jsx
+++ src/pages/notFoundPage/NotFound.jsx
@@ -1,4 +1,5 @@
-import { SmileyXEyes} from '@phosphor-icons/react';
+import { Link } from 'react-router-dom';
+import {CaretRight, SmileyXEyes} from '@phosphor-icons/react';
 import Header from '../../components/header/Header.jsx';
 import SectionDivider from '../../components/misc/sectionDivider/SectionDivider.jsx';
 import './NotFound.css';
@@ -13,8 +14,25 @@ function NotFound() {
                 icon={<SmileyXEyes className="header__icon--big"/>}
             />
             <section className="outer-content-container">
-                <div className="inner-content-container__column">
+                <div className="inner-content-container__column not-found-section">
                     <SectionDivider title="Not Found"/>
+                    <h4 className="default-text-restrictor">
+                        Lost your way? No problem! Let&apos;s get you back on the right track to discover more delicious recipes! 🚀🌮
+                    </h4>
+                    <div className="not-found__links-wrapper">
+                        <Link to="/" className="go-to-link">
+                            <CaretRight size={22} />
+                            Take me back to the Home Page
+                        </Link>
+                        <Link to="/recipe-quiz" className="go-to-link">
+                            <CaretRight size={22} />
+                            Do the Recipe Quiz to uncover your next favorite meal
+                        </Link>
+                        <Link to="/all-recipes" className="go-to-link">
+                            <CaretRight size={22} />
+                            Go to our recipe collection to explore new recipes
+                        </Link>
+                    </div>
                 </div>
             </section>
         </>
