ed3ef5cf4a42adbf348d4367b001681025f0ad30
AnneKluytmans <annekluytmans1@gmail.com>
2024-08-22 15:50:30 +0200

    Implemented Terms and Policy page with header and global structure

--- src/pages/termsAndPolicyPage/TermsAndPolicy.jsx
+++ src/pages/termsAndPolicyPage/TermsAndPolicy.jsx
@@ -1,9 +1,22 @@
+import { FileText } from '@phosphor-icons/react';
+import Header from '../../components/header/Header.jsx';
+import SectionDivider from '../../components/misc/sectionDivider/SectionDivider.jsx';
 import './TermsAndPolicy.css';
 
 function TermsAndPolicy() {
     return (
         <>
-            <h1>Terms and Policy</h1>
+            <Header
+                title="Terms & Policy"
+                subtitle="Your trust matters: know our terms and privacy policy"
+                icon={<FileText className="header__icon"/>}
+            />
+            <section className="outer-content-container">
+                <div className="inner-content-container__column">
+                    <SectionDivider title="User Terms"/>
+                    <SectionDivider title="Privacy Policy"/>
+                </div>
+            </section>
         </>
     );
 }
