524e96c4bc6b61545c802dfcca4627131954511c
AnneKluytmans <annekluytmans1@gmail.com>
2024-08-23 11:14:35 +0200

    Added Edamam Badge attribution to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { LineVertical, InstagramLogo, FacebookLogo } from '@phosphor-icons/react';
+import EdamamBadge from '../../assets/icons/edamam-badge.svg?react';
 import './Footer.css';
 
 
@@ -12,12 +13,15 @@ function Footer( { className } ) {
                     <li><Link to="/faq">Faq</Link></li>
                     <li><Link to="/terms-and-policy">Terms & Policy</Link></li>
                 </ul>
-                <div className="footer__text-wrapper">
-                    <p>Inspiring recipes at <em>&copy;</em> Meal Match 2024</p>
-                    <span className="footer__text-designer">
-                        <LineVertical size={24} color="#012F1E"/>
-                        <p>Designed by Anne Kluijtmans</p>
-                    </span>
+                <div className="footer__attribution-wrapper">
+                    <div className="footer__text-wrapper">
+                        <p>Inspiring recipes at <em>&copy;</em> Meal Match 2024</p>
+                        <span className="footer__text-designer">
+                            <LineVertical size={24} color="#012F1E"/>
+                            <p>Designed by Anne Kluijtmans</p>
+                        </span>
+                    </div>
+                    <EdamamBadge className="footer__edamam-badge"/>
                 </div>
                 <div className="footer__social-links-wrapper">
                     <a href="https://www.instagram.com/everynightoftheweek/?utm_source=ig_embed&ig_rid=eac5add9-016b-47c5-8089-4c1c4fe7b472"
